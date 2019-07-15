b46935bfa9f858924cb592cb7014f5cadcdea910
anupamagarg <dolly.singla@gmail.com>
2019-07-14 21:48:27 -0500

    Async promise

---
+++ Promises.js
@@ -0,0 +1,24 @@
+const posts = [
+    { title: 'Post One', body: 'This is post one' },
+    { title: 'Post Two', body: 'This is post two' }
+  ];
+  
+  function getPosts() {
+    setTimeout(() => {
+      let output = '';
+      posts.forEach((post, index) => {
+        output += `<li>${post.title}</li>`;
+      });
+      document.body.innerHTML = output;
+    }, 1000);
+  }
+
+  function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            
+          }, 2000);
+    });
+   
+  }
